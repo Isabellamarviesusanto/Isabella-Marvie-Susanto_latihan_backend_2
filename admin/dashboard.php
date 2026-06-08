9d6b69553b2181a1666e464190c9b130bc1b8a59
Isabellamarviesusanto <isabellamarviesusanto@itbss.ac.id>
2026-06-08 17:36:02 +0700

    part 3

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -6,7 +6,7 @@ echo "welcome, ".$username;
 <br>
 <a href="courses/index.php">manajemen kelas</a>
 <br>
-<a href="registrations/index.php">manajemen kelas</a>
+<a href="registrations/index.php">manajemen pendaftaran</a>
 <br>
 <a href="logout.php">logout</a>
 
