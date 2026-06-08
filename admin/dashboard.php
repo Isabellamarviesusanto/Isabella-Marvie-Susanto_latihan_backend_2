a769eebc9a77085a677dba9a52072b4bc4ccfb16
Isabellamarviesusanto <isabellamarviesusanto@itbss.ac.id>
2026-06-08 17:33:53 +0700

    courses

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -4,6 +4,8 @@ include "security.php";
 echo "welcome, ".$username;
 ?>
 <br>
+<a href="courses/index.php">manajemen kelas</a>
+<br>
 <a href="registrations/index.php">manajemen kelas</a>
 <br>
 <a href="logout.php">logout</a>
